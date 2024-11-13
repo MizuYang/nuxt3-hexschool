7a62cce70b83e2fd3705de0f04a663ce75e09d46
Mizu <psdk55052004@gmail.com>
2024-11-13 13:56:20 +0800

    新增 Day1: 建立 Nuxt3 專案

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup>
+const route = useRoute()
+
+console.log('route: ', route)
+</script>
+
+<template>
+  Page: 首頁 <br />
+
+  目前路由的路徑 {{ route.fullPath }}
+</template>
+
+<style lang='scss' scope></style>
