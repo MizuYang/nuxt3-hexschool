ddc1aa445c24f356b5cd40b0c405724d745a39bd
Mizu <psdk55052004@gmail.com>
2024-11-19 09:42:43 +0800

    新增 weeks2: 嵌套路由

---
+++ pages/admin/A.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>Admin A</div>
+</template>
+
+<style lang='scss' scope></style>
+
---
+++ pages/admin/B.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>Admin B</div>
+</template>
+
+<style lang='scss' scope></style>
+
