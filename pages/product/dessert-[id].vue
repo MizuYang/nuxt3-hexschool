fabbe5e997b0ddb3289c0aa77c6695a75560a266
mizu <psdk55052004@gmail.com>
2024-11-25 22:56:14 +0800

    新增 week3: useSeoMeta

--- pages/product/dessert-[id].vue
+++ pages/product/dessert-[id].vue
@@ -1,18 +1,31 @@
 <script setup>
 const route = useRoute();
 
-const name = decodeURIComponent(route.fullPath.split('-')[1]);
+const name = decodeURIComponent(route.fullPath.split("-")[1]);
 
-useHead({
-  // useHead 的所有屬性,可參考：https://nuxt.com/docs/api/composables/use-head#usehead
+// useHead
+// useHead({
+//   // useHead 的所有屬性,可參考：https://nuxt.com/docs/api/composables/use-head#usehead
 
-  title: `甜點名稱：${name}`,
-  meta: [
-    { name: "description", content: `useHead SEO description 甜點: ${name}` },
-    { property: "og:title", content: `useHead SEO og:title 甜點: ${name}` },
-    { property: "og:url", content: `useHead SEO og:url 甜點: ${name}` },
-    { property: "og:description", content: `useHead SEO og:description 甜點: ${name}` },
-  ],
+//   title: `甜點名稱：${name}`,
+//   meta: [
+//     { name: "description", content: `useHead SEO description 甜點: ${name}` },
+//     { property: "og:title", content: `useHead SEO og:title 甜點: ${name}` },
+//     { property: "og:url", content: `useHead SEO og:url 甜點: ${name}` },
+//     { property: "og:description", content: `useHead SEO og:description 甜點: ${name}` },
+//   ],
+// });
+
+// useSeoMeta server, client 都會動態更新 seo meta tag
+useSeoMeta({
+  title: `useSeoMeta-title-甜點名稱: ${name}`,
+  description: `useSeoMeta-description-甜點名稱: ${name}`,
+  ogDescription: `useSeoMeta-ogDescription-甜點名稱: ${name}`,
+  ogTitle: `useSeoMeta-ogTitle-甜點名稱: ${name}`,
+  //  ogImage: "",
+  //  twitterCard: "summary_large_image",
+  //  twitterSite: "",
+  //  twitterCreator: "",
 });
 </script>
 
