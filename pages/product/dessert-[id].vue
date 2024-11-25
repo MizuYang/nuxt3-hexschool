1402beac72044928727e48071d07f8df36b7010b
mizu <psdk55052004@gmail.com>
2024-11-26 00:36:28 +0800

    新增 week3: useHead 在單一元件中載入外部資源

--- pages/product/dessert-[id].vue
+++ pages/product/dessert-[id].vue
@@ -40,7 +40,21 @@ const name = decodeURIComponent(route.fullPath.split("-")[1]);
 //   //  twitterCreator: "",
 // });
 
-
+// 在元件中載入外部資源
+// useHead({
+//   link: [
+//     {
+//       rel: "stylesheet",
+//       href: "https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css",
+//     },
+//   ],
+//   script: [
+//     {
+//       src: "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js",
+//       async: true,
+//     },
+//   ],
+// });
 </script>
 
 <template>
