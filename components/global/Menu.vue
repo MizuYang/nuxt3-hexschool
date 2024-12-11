09ecb93a0f2ba0d6dc2bf3c280316f5f88b6de58
Mizu <psdk55052004@gmail.com>
2024-12-11 09:15:28 +0800

    新增 拆 <Menu> 元件

--- components/global/Menu.vue
+++ components/global/Menu.vue
@@ -1,5 +1,22 @@
 <template>
   <span>
-    選單
+    <NuxtLink to="/" activeClass="active">最外層首頁</NuxtLink> |
+    <NuxtLink to="/user" activeClass="active">user</NuxtLink> |
+    <NuxtLink to="/user/A" activeClass="active">user/A</NuxtLink> |
+    <NuxtLink to="/user/B" activeClass="active">user/B</NuxtLink> |
+    <NuxtLink to="/front" activeClass="active">front</NuxtLink> |
+    <NuxtLink to="/front/xxx1" activeClass="active">front/xxx1/index</NuxtLink>
+    | <NuxtLink to="/admin" activeClass="active">admin</NuxtLink> |
+    <NuxtLink to="/admin/A" activeClass="active">adminA</NuxtLink> |
+    <NuxtLink to="/admin/B" activeClass="active">adminB</NuxtLink> |
+    <NuxtLink to="/product" activeClass="active">product</NuxtLink> |
+    <NuxtLink to="/product/dessert-泡芙" activeClass="active"
+      >dessert-泡芙</NuxtLink
+    >
+    |
+    <NuxtLink to="/product/dessert-舒芙蕾" activeClass="active"
+      >dessert-舒芙蕾</NuxtLink
+    >
+    |
   </span>
 </template>
--- layouts/mainLayout.vue
+++ layouts/mainLayout.vue
@@ -1,24 +1,11 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
-  <NuxtLink to="/" activeClass="active">最外層首頁</NuxtLink> |
-  <NuxtLink to="/user" activeClass="active">user</NuxtLink> |
-  <NuxtLink to="/user/A" activeClass="active">user/A</NuxtLink> |
-  <NuxtLink to="/user/B" activeClass="active">user/B</NuxtLink> |
-  <NuxtLink to="/front" activeClass="active">front</NuxtLink> |
-  <NuxtLink to="/front/xxx1" activeClass="active">front/xxx1/index</NuxtLink> |
-  <NuxtLink to="/admin" activeClass="active">admin</NuxtLink> |
-  <NuxtLink to="/admin/A" activeClass="active">adminA</NuxtLink> |
-  <NuxtLink to="/admin/B" activeClass="active">adminB</NuxtLink> |
-  <NuxtLink to="/product" activeClass="active">product</NuxtLink> |
-  <NuxtLink to="/product/dessert-泡芙" activeClass="active">dessert-泡芙</NuxtLink> |
-  <NuxtLink to="/product/dessert-舒芙蕾" activeClass="active">dessert-舒芙蕾</NuxtLink> |
+  <Menu />
 
   <header>主要的表頭</header>
   <slot />
   <footer>主要的表尾</footer>
 </template>
 
-<style lang='scss' scope></style>
+<style lang="scss" scope></style>
