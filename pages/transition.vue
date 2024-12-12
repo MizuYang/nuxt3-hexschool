415da3da4a23590b40e8638c2eb474b888f81be1
Mizu <psdk55052004@gmail.com>
2024-12-12 10:03:36 +0800

    新增 week5: GSAP 動畫

--- pages/transition.vue
+++ pages/transition.vue
@@ -1,12 +1,19 @@
 <script setup>
+import { gsap } from "gsap";
+import { Flip } from "gsap/Flip";
+gsap.registerPlugin(Flip);
+
 const isShow = ref(true);
 const nums = ref([]);
+const gsapNums = ref([]);
+let gsapState = [];
 
 getNumber(50);
 
 function getNumber(count = 50) {
   for (let i = 0; i < count; i++) {
     nums.value[i] = i + 1;
+    gsapNums.value[i] = i + 1;
   }
 }
 function getNumReset() {
@@ -15,6 +22,73 @@ function getNumReset() {
 }
 function sortRandomNumber() {
   nums.value.sort(() => (Math.floor(Math.random() * 2) ? 1 : -1));
+}
+async function sortGsapFlip() {
+  // 先記入改變前的狀態
+  gsapState = Flip.getState(".item");
+
+  // 改變元素的狀態 (修改順序就是改變元素的狀態)
+  gsapNums.value.sort(() => (Math.floor(Math.random() * 2) ? 1 : -1));
+
+  // 等到 DOM 元素重新 render 後，才能執行 GSAP 動畫
+  await nextTick();
+
+  // GSAP 動畫、配置
+  Flip.from(gsapState, {
+    duration: 0.5,
+    absolute: true,
+    duration: 0.8,
+    ease: "expo.inOut",
+    spin: true,
+    scale: true,
+  });
+}
+async function resetGsapFlip() {
+  const state = Flip.getState(".item");
+  gsapNums.value = [];
+  getNumber(50);
+  if (gsapState?.targets?.length) {
+    await nextTick();
+    Flip.from(state, {
+      duration: 0.5,
+      absolute: true,
+    });
+  }
+  gsapState = []; // 初始化
+}
+async function rotate() {
+  const state = Flip.getState(".item");
+  const items = [...document.querySelectorAll(".item")];
+  items.forEach((item) => (item.style.width = "20px"));
+  setTimeout(async () => {
+    const state = Flip.getState(".item");
+    items.forEach((item) => (item.style.width = "59px"));
+    await nextTick();
+    Flip.from(state, {
+      absolute: true,
+      duration: 0.8,
+      spin: true,
+    });
+  }, 2000);
+  setTimeout(async () => {
+    const state = Flip.getState(".item");
+    items.forEach((item) => (item.style.width = "100px"));
+    await nextTick();
+    Flip.from(state, {
+      absolute: true,
+      duration: 0.8,
+      spin: true,
+    });
+  }, 1000);
+  // if (gsapState?.targets?.length) {
+  await nextTick();
+  Flip.from(state, {
+    absolute: true,
+    duration: 0.8,
+    ease: "expo.inOut",
+    spin: true,
+  });
+  // }
 }
 </script>
 
@@ -30,27 +104,27 @@ function sortRandomNumber() {
     </section>
 
     <section class="random-num-wrap">
-      <h2>使用 {{ '<transitionGrop>' }} 製作動畫</h2>
-        <ul class="random-num-list">
-          <transition-group name="random-num">
-            <template v-for="num in nums" :key="`transitiongrop-${num}`">
-              <li>
-                {{ num }}
-              </li>
-            </template>
-          </transition-group>
+      <h2>使用 transitionGrop 製作動畫</h2>
+      <ul class="random-num-list">
+        <transition-group name="random-num">
+          <template v-for="num in nums" :key="`transitiongrop1-${num}`">
+            <li>
+              {{ num }}
+            </li>
+          </template>
+        </transition-group>
       </ul>
       <button type="button" @click="sortRandomNumber" class="me-2">
         亂數排序
       </button>
       <button type="button" @click="getNumReset">重置</button>
     </section>
-    
+
     <section class="random-num-wrap mt-0">
       <h2>使用 auto-animate 製作動畫</h2>
-        <ul class="random-num-list" v-auto-animate>
-          <template v-for="num in nums" :key="`transitiongrop-${num}`">
-            <li>
+      <ul class="random-num-list" v-auto-animate>
+        <template v-for="num in nums" :key="`transitiongrop2-${num}`">
+          <li>
             {{ num }}
           </li>
         </template>
@@ -59,6 +133,20 @@ function sortRandomNumber() {
         亂數排序
       </button>
       <button type="button" @click="getNumReset">重置</button>
+    </section>
+
+    <section class="random-num-wrap mt-0">
+      <h2>使用 GSAP 製作動畫</h2>
+      <ul class="random-num-list gsap-flip-test" style="min-height: 132px">
+        <template v-for="num in gsapNums" :key="`transitiongropGsap-${num}`">
+          <li class="item">
+            {{ num }}
+          </li>
+        </template>
+      </ul>
+      <button type="button" @click="sortGsapFlip" class="me-2">亂數排序</button>
+      <button type="button" @click="resetGsapFlip">重置</button>
+      <button type="button" @click="rotate">旋轉</button>
     </section>
   </div>
 </template>
